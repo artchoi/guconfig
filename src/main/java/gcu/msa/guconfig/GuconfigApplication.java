c9e9deba77675c7b1aee3620a2e59fc559e9cf78
artchoi <artchoi@gmail.com>
2024-04-08 07:52:58 +0900

    config

---
+++ src/main/java/gcu/msa/guconfig/GuconfigApplication.java
@@ -0,0 +1,13 @@
+package gcu.msa.guconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuconfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gcu/msa/guconfig/GuconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package gcu.msa.guconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
